05e1dbe7aa1ee70d59f7b49de531e276c466db75
Chris Zuniga <zunigachristian81@gmail.com>
2021-09-17 13:27:25 -0500

    questions&choices

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        question: "What doest 'CSS' stand for?",
+        choices: ["a. Customer Service and Support", "b. Central Security Service", "c. Cascading Style Sheets", "d. Commander Support Staff"],
+        answer: "c. Cascading Style Sheets"
+    },
+    {
+        question: "Which best describes a Boolean?",
+        choices:["a. True/False", "b. A sandwich", "c. HTML element", "d. CSS selector"],
+        answer: "a. True/False"        
+    },
+    {
+        question: "Who invented JavaScript?",
+        choices: ["a. Douglas Crockford", "b. Sheryl Sandberg", "c. Brendan Eich", "d. Ben Javascript"],
+        answer: "c. Brendan Eich"        
+    },
+    {
+        question: "How do you add a comment in a HTML?",
+        choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
+        answer: "b. <!--This is a comment-->"        
+    },
+    {
+        question: "The first index of an array is ____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        answer: "a. 0"        
+    }
+];
