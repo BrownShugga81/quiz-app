68d0bbb0924ee20c91c2826eacbfa9874440c8b1
Chris Zuniga <zunigachristian81@gmail.com>
2021-09-23 13:56:49 -0500

    begin button clears page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,45 @@
 const questions = [
     {
-        question: "What does 'CSS' stand for?",
-        choices: ["a. Customer Service and Support", "b. Central Security Service", "c. Cascading Style Sheets", "d. Commander Support Staff"],
+        quest: "What does 'CSS' stand for?",
+        options: ["a. Customer Service and Support", "b. Central Security Service", "c. Cascading Style Sheets", "d. Commander Support Staff"],
         answer: "c. Cascading Style Sheets"
     },
     {
-        question: "Which best describes a Boolean?",
-        choices:["a. True/False", "b. A sandwich", "c. HTML element", "d. CSS selector"],
+        quest: "Which best describes a Boolean?",
+        options:["a. True/False", "b. A sandwich", "c. HTML element", "d. CSS selector"],
         answer: "a. True/False"        
     },
     {
-        question: "Who invented JavaScript?",
-        choices: ["a. Douglas Crockford", "b. Sheryl Sandberg", "c. Brendan Eich", "d. Ben Javascript"],
+        quest: "Who invented JavaScript?",
+        options: ["a. Douglas Crockford", "b. Sheryl Sandberg", "c. Brendan Eich", "d. Ben Javascript"],
         answer: "c. Brendan Eich"        
     },
     {
-        question: "How do you add a comment in a HTML?",
-        choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
+        quest: "How do you add a comment in a HTML?",
+        options: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
         answer: "b. <!--This is a comment-->"        
     },
     {
-        question: "The first index of an array is ____.",
-        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        quest: "The first index of an array is ____.",
+        options: ["a. 0", "b. 1", "c. 8", "d. any"],
         answer: "a. 0"        
     }
 ];
 
-var beginQuiz = document.getElementById("beginBtn");
 
-function beginQuiz () {
+
+
+function beginquiz() {
+   console.log("begin");
 
 }
 
 
 
-beginBtn.addEventListener("click", beginQuiz);
+beginBtn.addEventListener("click", beginquiz);
+
+
+// clear starting page&begin first quest
+beginBtn.addEventListener("click", function(){
+document.getElementById("display").innerHTML = "";
+});
